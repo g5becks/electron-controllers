8eadd1ca0b2f87a0c1d2ee462b7b09556573bfc3
G.Becks <techstar.dev@hotmail.com>
2020-02-02 20:36:18 -0500

    update tsconfig.json

--- test/handler.test.ts
+++ test/handler.test.ts
@@ -5,7 +5,7 @@ test('handlers should return proper response', async () => {
   // eslint-disable-next-line @typescript-eslint/require-await
   const myAction: IpcAction<string, string> = async arg => `my ${arg}`
   const request: IpcRequest<string> = { responseChannel: '', payload: 'testing' }
-  const testHandler = createHandler(myAction)
+  const testHandler = createHandler('',myAction)
   const response = await testHandler.makeResponse(request)
   expect(response).toBe(`my ${request.payload}`)
 })
